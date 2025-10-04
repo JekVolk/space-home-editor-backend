c350f53acffe54862e3b7925e54fd42b9ddb234f
Євгеній <137955860+JekVolk@users.noreply.github.com>
2025-10-04 16:57:30 +0300

    Develop (#7)
    * feat: Переніс db в нову деректорію db

--- space_home_editor/settings.py
+++ space_home_editor/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'space_home_editor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
     }
 }
 
